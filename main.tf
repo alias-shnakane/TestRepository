4966f7821ac8b575b040b9b16afafb958eab2f69
shnakane <shnakane@amazon.co.jp>
2022-06-01 18:49:17 +0900

    main

--- main.tf
+++ main.tf
@@ -1,17 +1,63 @@
-variable "example_instance_type" {
-  default = "t3.micro"
+# Provider
+provider "aws" {
+  region = "ap-northeast-1"
+}
+
+# data
+data "aws_ami" "recent_amazon_linux_2" {
+  most_recent = true
+  owners = ["amazon"]
+
+  filter {
+    name = "name"
+    values = ["amzn2-ami-hvm-2.0.????????-x86_64-gp2"]
+  }
+
+  filter {
+    name = "state"
+    values = ["available"]
+  }
+}
+
+# local変数
+locals {
+  example_instance_type = "t3.micro"
 }
 
 
+# resource security group
+resource "aws_security_group" "example_ec2" {
+  name = "example-ec2"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# resource instance
 resource "aws_instance" "example" {
-  ami = "ami-0c3fd0f5d33134a76"
-  instance_type = var.example_instance_type
-  
-  user_data = <<EOF
-#!bin/bash
-yum install -y httpd
-systemctl start httpd.service
-EOF
+  ami                    = data.aws_ami.recent_amazon_linux_2.image_id
+  instance_type          = local.example_instance_type
+  vpc_security_group_ids = [aws_security_group.example_ec2.id]
+
+  user_data = file("./user_data.sh)
 }
 
 
+output "example_instance_id" {
+  value = aws_instance.example.id
+}
+
+output "example_public_dns"{
+  value = aws_instance.example.example_public_dns
+}
