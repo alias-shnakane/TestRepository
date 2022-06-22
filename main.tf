6cd93db873b73287b78f2c60a1e282a9c8b3b24f
Shuichiro Nakane <shuichironakane0906@gmail.com>
2022-06-22 17:37:24 +0900

    20220622

--- main.tf
+++ main.tf
@@ -1,12 +1,15 @@
-provider "aws" {
-  region = "ap-northeast-1"
+# iam policy document
+data "aws_iam_policy_document" "allow_describe_regions" {
+  statement {
+    effect    = "Allow"
+    actions   = ["ec2:DescribeRegions"]
+    resources = ["*"]
+  }
 }
 
-module "web_server" {
-  source = "./http_server"
-  instance_type = "t3.micro"
-}
-
-output "public_dns" {
-  value = module.web_server.public_dns
+module "describe_regions_for_ec2" {
+  source     = "./iam_role"
+  name       = "describe-regions-for-ec2"
+  identifier = "ec2.amazonaws.com"
+  policy     = data.aws_iam_policy_document.allow_describe_regions.json
 }
