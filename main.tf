cf67078d6cf177976ede5f4c10651fca60e1dca4
Shuichiro Nakane <shuichironakane0906@gmail.com>
2022-06-22 18:20:22 +0900

    20220622-6

--- main.tf
+++ main.tf
@@ -7,9 +7,9 @@ data "aws_iam_policy_document" "allow_describe_regions" {
   }
 }
 
-module "describe_regions_for_ec2" {
+module "describe_regions_for_ec2_2" {
   source     = "./iam_role"
-  name       = "describe-regions-for-ec2"
+  name       = "describe-regions-for-ec2_2"
   identifier = "ec2.amazonaws.com"
   policy     = data.aws_iam_policy_document.allow_describe_regions.json
 }
